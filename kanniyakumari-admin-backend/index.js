507acdf77b77cf15b046408e3346b2d789a5155e
ROSHINTH Sojan <142890245+Rosi8870@users.noreply.github.com>
2026-01-18 22:02:07 +0530

    package json

--- kanniyakumari-admin-backend/index.js
+++ kanniyakumari-admin-backend/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import adminProblems from "./routes/problems.routes.js";
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/admin/problems", adminProblems);
 
 app.get("/", (req, res) => {
   res.json({ status: "Admin backend running ✅" });
 });
 
-app.listen(process.env.PORT || 5001);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Admin backend running on port", PORT);
+});
